1324fd96ddf12378c8972142d829cf61c6a0c365
IsmaelAquino <ibranco@brasiliano.com.br>
2022-08-04 16:31:37 -0300

    feat: Create filmes controller

---
+++ FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        private List<Filme> filmes = new List<Filme>();
+        
+        [HttpPost]
+        public void AddMovie(Filme addMovieDto)
+        {
+            Console.WriteLine(addMovieDto.Title);
+        }
+    }
+}
