524125a28c402d7a2d166df12ea4b9fe487631ce
IsmaelAquino <ibranco@brasiliano.com.br>
2022-08-04 16:31:48 -0300

    feat: Create Filme model

---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+}
